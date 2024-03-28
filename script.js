59f0496e38a281a2b54b51164bcff029ede6dcfb
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-03-28 20:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function shortcut(s1, s2) {
   // your code here
+	if (!s1 || !s2) {
+		return "";
+	}
+	var initialLetters = s1.charAt(0) + s2.charAt(0);
+	return initialLetters;
 }
 
-// Do not change the code below.
+//Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
